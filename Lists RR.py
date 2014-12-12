1999859d46037794c1428e696a1ceff2752794b3
Hugo Bahman <41408@longroad.ac.uk>
2014-12-12 14:08:47 +0000

    Starting Lists

---
+++ Lists RR.py
@@ -0,0 +1,27 @@
+#Hugo
+#Lists R&R - Q1
+
+def getNames():
+    for count in range (8):
+        if count = 1:
+            1 = input("Please enter the name of the first student: ")
+        elif count = 2:
+            2 = input("Please enter the name of the second student: ")
+        elif count = 3:
+            3 = input("Please enter the name of the third student: ")
+        elif count = 4:
+            4 = input("Please enter the name of the fourth student: ")
+        elif count = 5:
+            5 = input("Please enter the name of the fifth student: ")
+        elif count = 6:
+            6 = input("Please enter the name of the sixth student: ")
+        elif count = 7:
+            7 = input("Please enter the name of the seventh student: ")
+        elif count = 8:
+            8 = input("Please enter the name of the eigth student: ")
+        return 1,2,3,4,5,6,7,8
+
+def select_student(1,2,3,4,5,6,7,8):
+    for count in range (8):
+        
+        
